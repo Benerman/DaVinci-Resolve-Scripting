57361a9fb90dafe83791f5739b526957a2b084c5
Benerman <48377757+Benerman@users.noreply.github.com>
2019-06-05 00:36:07 -0400

    updates

--- resolve_class_build.py
+++ resolve_class_build.py
@@ -13,6 +13,7 @@ class R():
 	ms		= resolve.GetMediaStorage()
 	proj    = pm.GetCurrentProject()
 	mp      = proj.GetMediaPool()
+	timeln 	= proj.GetCurrentTimeline()
 	folder  = mp.GetRootFolder()
 	cfolder = mp.GetCurrentFolder()
 	clips   = folder.GetClips()
@@ -26,21 +27,23 @@ class R():
 			print("Error: Ensure Resolve is running while this script executes\nScript will close now")
 			sys.exit()
 
+
 	def get_current_project(self):
-		return self.pm.GetCurrentProject()
+		print(self.proj.GetName())
+		return self.proj.GetCurrentTimeline()
+
 
 	def set_project_name(self, project_name):
 		return self.pm.CreateProject(project_name)
 
-	def add_new_folder(self, folder_name):
-		print('Added {} folder to rootFolder'.format(folder_name))
-		return self.mp.AddSubFolder(folder, folder_name)
 
-	def set_root_folder(self):
+	def get_root_folder(self):
 		return self.mp.GetRootFolder()
+
 
 	def get_current_folder(self):
 		return self.mp.GetCurrentFolder()
+
 
 	def set_current_folder(self, folder):
 		"""returns Bool"""
@@ -50,20 +53,41 @@ class R():
 			return self.mp.SetCurrentFolder(cfolder)
 		return self.mp.SetCurrentFolder(folder)
 
+
 	def get_sub_folders(self):
 		self.folder = self.mp.GetRootFolder()
 		return self.folder.GetSubFolders()
 
 
 	def get_folder_name(self):
-		# self.cfolder = self.mp.GetCurrentFolder()
+		print(self.cfolder.GetName())
 		return self.cfolder.GetName()
 
 
-	def add_sub_folder(self, folder_name='new folder', parent_folder=None):
+	def add_new_folder(self, folder_name):
+		"""
+		add_new_folder('folder_name')
+
+		### Adding Folders
+		'Master' > 'Test1'
+		R.add_new_folder("Test1") ## Always Adding 1 folder to Root level under 'Master'
+		
+		"""
+		print('Added {} folder to rootFolder'.format(folder_name))
+		return self.mp.AddSubFolder(self.folder, folder_name)
+
+
+	def add_sub_folder(self, folder_name, parent_folder=None):
 		"""
 		Add a subfolder to a created parent folder
-		add_sub_folder(String of folder name, Pass a parent folder)
+		add_sub_folder(String of folder name, Pass a parent Folder object(Optional))
+		
+		add_sub_folder("Test2") ## Adding 1 folder to most recent created folder or 'Master' if no folders exist
+		.. > 'Test2' or 'Master' > 'Test2'
+
+		### Adding Sub folder named 'test8' to parent folder 'test7' to parent folder 'Test2'
+		'Master' > 'Test2' > 'test7' > 'test8'
+		R.add_sub_folder("test8", R.add_sub_folder("test7", R.add_new_folder("Test2")))
 		"""
 		self.folder = self.mp.GetRootFolder()
 		self.cfolder = self.mp.GetCurrentFolder()
@@ -71,7 +95,7 @@ class R():
 		if parent_folder == None:
 			return self.mp.AddSubFolder(self.cfolder, folder_name)
 		self.reveal_in_storage(parent_folder)
-		# setcurrentFolder = self.mp.SetCurrentFolder(parent_folder)
+		setcurrentFolder = self.mp.SetCurrentFolder(parent_folder)
 		if setcurrentFolder == 'None':
 			print(setcurrentFolder)
 			addFolder = self.mp.AddSubFolder(self.cfolder, parent_folder)
@@ -84,15 +108,57 @@ class R():
 	def media_sub_folders(self, folderPath):
 		return self.ms.GetSubFolders(folderPath)
 
+
 	def reveal_in_storage(self, path):
 		return self.ms.RevealInStorage(path)
 
 
+	def get_current_timeline(self):
+		print(self.timeln.GetName())
+		return self.proj.GetCurrentTimeline()
 
 
-R = R()		
+	def get_timeline_name(self):
+		print(self.timeln.GetName())
+		return self.timeln.GetName()
 
-R.add_sub_folder("test1", 'Master')
+
+	def select_timeline(self, idx):
+		print("There are {} timelines in this project.\nYou selected timeline number {}".format(int(self.proj.GetTimelineCount()),idx))
+		return self.proj.SetCurrentTimeline(self.proj.GetTimelineByIndex(idx))
+
+
+R = R()		#instantiate the Class
+
+# list1 = R.get_sub_folders()
+# print(list1)
+# print(list1.values())
+# print(R.get_folder_name())
+
+# R.get_current_timeline()
+# R.get_current_project()
+# R.get_timeline_name()
+
+# R.select_timeline(1)
+# R.proj.GetTimelineByIndex(1)
+
+
+# #Get Timeline Names and Objects  ----- Not Working
+# timeline_count = int(R.proj.GetTimelineCount())
+# timelinenames = []
+# for i in range(timeline_count+1):
+# 	R.proj.SetCurrentTimeline(i)
+# 	R.get_timeline_name()
+# 	R.proj.GetTimelineByIndex(i)
+# 	print(R.proj.GetTimelineByIndex(i))
+
+R.get_folder_name()
+# R.get_root_folder()
+
+### Adding Folders
+# R.add_new_folder("Test1") ## Always Adding 1 folder to Root level under 'Master'
+### 'Master' > 'Test1'
+
 
 
 # if __name__ == '__main__':
@@ -101,7 +167,7 @@ R.add_sub_folder("test1", 'Master')
 # 	resolve = DaVinciResolveScript.scriptapp('Resolve')
 # 	fusion 	= DaVinciResolveScript.scriptapp('Fusion')
 # 	pm      = resolve.GetProjectManager()
-#	  ms		  = resolve.GetMediaStorage()
+#	ms		= resolve.GetMediaStorage()
 # 	proj    = pm.GetCurrentProject()
 # 	mp      = proj.GetMediaPool()
 # 	folder  = mp.GetRootFolder()
